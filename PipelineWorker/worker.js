9f9e20d671c2555d7342467aec655a8f681d81be
amiturgman <ami.turgman@microsoft.com>
2016-01-18 17:03:52 +0200

    initial version

---
+++ PipelineWorker/worker.js
@@ -0,0 +1,12 @@
+﻿
+console.log('env variables', process.env);
+
+var db = require('./db');
+
+
+setInterval(function () {
+    console.log('executing');
+    db.executeStatement1(function (err) { 
+        if (err) console.error('error:', err);
+    });
+}, 5000);
