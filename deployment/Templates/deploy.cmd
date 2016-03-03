5e8fdb176ab6d08af9918fa0feb240e37e0885d2
morsh <mor.shemesh@gmail.com>
2016-03-03 19:19:51 +0200

    updating sql deloyment script

--- deployment/Templates/deploy.cmd
+++ deployment/Templates/deploy.cmd
@@ -1,9 +1,9 @@
 ﻿REM azure group create -n [resource group name] -l "West US"
-azure group create -n miroculus-pipeline -l "West US"
+call azure group create -n miroculus-pipeline -l "West US"
 
 REM azure group deployment create -f template.base.json -e parameters.test.json [resource group name] deployment-03012016-base
-azure group deployment create -f template.base.json -e parameters.test.json miroculus-pipeline deployment-03012016-base
+call azure group deployment create -f template.base.json -e parameters.test.json miroculus-pipeline deployment-03012016-base
 timeout 60
 
 REM azure group deployment create -f template.sourcecontrol.json -e parameters.test.json [resource group name] deployment-03012016-sourcecontrol
-azure group deployment create -f template.sourcecontrol.json -e parameters.test.json miroculus-pipeline deployment-03012016-sourcecontrol
+call azure group deployment create -f template.sourcecontrol.json -e parameters.test.json miroculus-pipeline deployment-03012016-sourcecontrol
--- ensure-sql.js
+++ ensure-sql.js
@@ -1,19 +1,20 @@
 ﻿var child_process = require('child_process');
 var path = require('path');
 
-var cmd = 'sqlcmd -U %DB_USER% -S %DB_SERVER% -P %DB_PASSWORD% -d master -Q "SELECT name FROM sys.databases WHERE name=\'%DB_NAME%1\'" -h -1';
-console.info('Searching for DB %s with user %s in server %s', process.env.DB_NAME, process.env.DB_USER, process.env.DB_SERVER);
+var cmd = 'sqlcmd -U %DB_USER% -S %DB_SERVER% -P %DB_PASSWORD% -d %DB_NAME% -Q ' +
+          '"SELECT * from INFORMATION_SCHEMA.TABLES WHERE table_name in(\'Documents\', \'Sentences\', \'Relations\', \'Entities\')" - h - 1 ';
+console.info('Searching for content in DB %s with user %s in server %s', process.env.DB_NAME, process.env.DB_USER, process.env.DB_SERVER);
 
 child_process.exec(cmd, null, function (error, stdout, stderr) {
   if (error) return console.error('Error querying for DBs', error);
   
   stdout = stdout || '';
+  if (stdout.indexOf('rows affected)') < 0) return console.error('there was a problem running the query');
   if (stdout.indexOf('(0 rows affected)') < 0) return console.info('db already exists');
   
   console.info('db not found, creating a new DB...');
   
   var createdb_command = '';
-  createdb_command += 'sqlcmd -U %DB_USER% -S %DB_SERVER% -P %DB_PASSWORD% -d master -Q "CREATE DATABASE %DB_NAME%" && ';
   createdb_command += 'sqlcmd -U %DB_USER% -S %DB_SERVER% -P %DB_PASSWORD% -d %DB_NAME% -i ' + path.join('Sql', 'sql.sql') + ' > createdb.log';
   child_process.exec(createdb_command, null, function (error, stdout, stderr) {
     
