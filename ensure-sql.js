9325b8bb7d4068c6d081317b50e3b8afb8fcc41e
morsh <mor.shemesh@gmail.com>
2016-03-03 18:38:53 +0200

    printing sql paramters in deployment script

--- ensure-sql.js
+++ ensure-sql.js
@@ -2,7 +2,8 @@
 var path = require('path');
 
 var cmd = 'sqlcmd -U %DB_USER% -S %DB_SERVER%.database.windows.net -P %DB_PASSWORD% -d master -Q "SELECT name FROM sys.databases WHERE name=\'%DB_NAME%1\'" -h -1';
-console.info(cmd);
+console.info('Searching for DB %s with user %s in server %s', process.env.DB_NAME, process.env.DB_USER, process.env.DB_SERVER);
+
 child_process.exec(cmd, null, function (error, stdout, stderr) {
   if (error) return console.error('Error querying for DBs', error);
   
