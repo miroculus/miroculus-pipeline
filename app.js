d6a6c6cf548166af6ac20f2a96f65857fbac4cfb
morsh <mor.shemesh@gmail.com>
2016-03-03 18:05:45 +0200

    adding deployment script

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-﻿console.log('Hello world'); 
+﻿var utils = require('./test/utils.js');
+var path = require('path');
+
+console.info('running script...')
+utils.runDBScript(path.join('Sql', 'sql.sql'), function (err) {
+  if (err) return console.error('Error while running the script:', err);
+  
+  console.info('completed Successfully');
+})
---
+++ ensure-sql.js
@@ -0,0 +1,22 @@
+﻿var child_process = require('child_process');
+var path = require('path');
+
+var cmd = 'sqlcmd -U %DB_USER% -S %DB_SERVER%.database.windows.net -P %DB_PASSWORD% -d master -Q "SELECT name FROM sys.databases WHERE name=\'%DB_NAME%1\'" -h -1';
+child_process.exec(cmd, null, function (error, stdout, stderr) {
+  if (error) return console.error('Error querying for DBs', error);
+  
+  stdout = stdout || '';
+  if (stdout.indexOf('(0 rows affected)') < 0) return console.info('db already exists');
+  
+  console.info('db not found, creating a new DB...');
+  
+  var createdb_command = '';
+  createdb_command += 'sqlcmd -U %DB_USER% -S %DB_SERVER%.database.windows.net -P %DB_PASSWORD% -d master -Q "CREATE DATABASE %DB_NAME%" && ';
+  createdb_command += 'sqlcmd -U %DB_USER% -S %DB_SERVER%.database.windows.net -P %DB_PASSWORD% -d %DB_NAME% -i ' + path.join('Sql', 'sql.sql') + ' > createdb.log';
+  child_process.exec(createdb_command, null, function (error, stdout, stderr) {
+    
+    if (error) return console.error('Error creating new DB', error);
+
+    return console.info('DB created successfully');
+  });
+});
