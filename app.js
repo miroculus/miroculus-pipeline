e4811b34665578d76d99f2d9ea078878afa69ec7
morsh <mor.shemesh@gmail.com>
2016-03-03 18:28:48 +0200

    remove code from app.js

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
-﻿var utils = require('./test/utils.js');
-var path = require('path');
-
-console.info('running script...')
-utils.runDBScript(path.join('Sql', 'sql.sql'), function (err) {
-  if (err) return console.error('Error while running the script:', err);
-  
-  console.info('completed Successfully');
-})
+﻿console.log('Hello world'); 
