486ff701864db109535efe4aa9e6abdc90e51dc3
amiturgman <ami.turgman@microsoft.com>
2016-03-06 17:47:22 +0200

    merging mor's work

--- deployment/Templates/deploy.cmd
+++ deployment/Templates/deploy.cmd
@@ -1,9 +1,6 @@
-﻿REM azure group create -n [resource group name] -l "West US"
-call azure group create -n miroculus-pipeline -l "West US"
+call azure group create -n miroculus-pipeline-amitu1 -l "West US"
 
-REM azure group deployment create -f template.base.json -e parameters.test.json [resource group name] deployment-03012016-base
-call azure group deployment create -f template.base.json -e parameters.test.json miroculus-pipeline deployment-03012016-base
-timeout 60
+call azure group deployment create -f template.base.json -e parameters.prod.json miroculus-pipeline-amitu1 deployment-03012016-base
+timeout 20
 
-REM azure group deployment create -f template.sourcecontrol.json -e parameters.test.json [resource group name] deployment-03012016-sourcecontrol
-call azure group deployment create -f template.sourcecontrol.json -e parameters.test.json miroculus-pipeline deployment-03012016-sourcecontrol
+call azure group deployment create -f template.sourcecontrol.json -e parameters.prod.json miroculus-pipeline-amitu1 deployment-03012016-sourcecontrol
