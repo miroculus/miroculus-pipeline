68701cb6675f27bcbc30ba89b031a4b6374210b6
morsh <mor.shemesh@gmail.com>
2016-03-07 11:28:40 +0200

    deployment moved to npm post\pre install

--- deployment/Templates/deploy.cmd
+++ deployment/Templates/deploy.cmd
@@ -1,6 +1,6 @@
-call azure group create -n miroculus-pipeline-amitu1 -l "West US"
+call azure group create -n miroculus-pipeline-morshe -l "West US"
 
-call azure group deployment create -f template.base.json -e parameters.prod.json miroculus-pipeline-amitu1 deployment-03012016-base
+call azure group deployment create -f template.base.json -e parameters.test.json miroculus-pipeline-morshe deployment-03012016-base
 timeout 20
 
-call azure group deployment create -f template.sourcecontrol.json -e parameters.prod.json miroculus-pipeline-amitu1 deployment-03012016-sourcecontrol
+call azure group deployment create -f template.sourcecontrol.json -e parameters.test.json miroculus-pipeline-morshe deployment-03012016-sourcecontrol
---
+++ deployment_modules/ensure-folder-hierarchy.js
@@ -0,0 +1,22 @@
+﻿var fs = require('fs');
+var path = require('path');
+
+console.info('Remove irrelevant folders');
+
+// Remove webjobs folder if this is a website
+if (process.env.DEPLOYMENT_ROLE !== 'website') {
+  
+  var serverPath = path.join(process.env.DEPLOYMENT_TARGET, 'server.js');
+  console.info('Deleting server.js from', serverPath);
+  
+  fs.unlinkSync(serverPath);
+} else {
+
+  // Remove website folder if this is a webjob
+  var appDataPath = path.join(process.env.DEPLOYMENT_TARGET, 'app_data');
+  console.info('Deleting web jobs folder', appDataPath);
+  
+  fs.rmdirSync(appDataPath);
+}
+
+console.info('Remove irrelevant folders completed');
