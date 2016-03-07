8b1b57ee14b4e1575b4e251a58bd2dc961dcb7f1
morsh <mor.shemesh@gmail.com>
2016-03-07 15:35:24 +0200

    fixing deployment and file deletion

---
+++ deployment_scripts/ensure-folder-hierarchy.js
@@ -0,0 +1,15 @@
+﻿var fs = require('fs');
+var path = require('path');
+
+console.info('Remove irrelevant folders');
+// Remove app.js of the web job if this is a website (this will remove the web job completely)
+if (process.env.DEPLOYMENT_ROLE === 'website') {
+
+  // Remove website app.js if this is a webjob
+  var appPath = path.join(process.env.DEPLOYMENT_TARGET, 'app_data', 'jobs', 'continuous', 'worker', 'app.js');
+  console.info('Deleting web jobs folder', appPath);
+  
+  if (fs.appPath(serverPath)) fs.unlinkSync(appPath);
+}
+
+console.info('Remove irrelevant folders completed');
