67bb7c9379d9804bd17839a320901fadcaffe9c5
amiturgman <ami.turgman@microsoft.com>
2016-03-08 17:21:26 +0200

    changing postInstall scripts

--- AzureDeployment/Templates/deploy.cmd
+++ AzureDeployment/Templates/deploy.cmd
@@ -1,6 +1,6 @@
-call azure group create -n miroculus-pipeline-morshe -l "West US"
+call azure group create -n miroculus-pipeline-prod -l "West US"
 
-call azure group deployment create -f azuredeploy.json -e parameters.test.private.json miroculus-pipeline-morshe deployment-03012016-base
+call azure group deployment create -f azuredeploy.json -e parameters.prod.private.json miroculus-pipeline-prod deployment-03012016-base
 timeout 20
 
-call azure group deployment create -f azuredeploy.sourcecontrol.json -e parameters.test.private.json miroculus-pipeline-morshe deployment-03012016-sourcecontrol
+call azure group deployment create -f azuredeploy.sourcecontrol.json -e parameters.prod.private.json miroculus-pipeline-prod deployment-03012016-sourcecontrol
